0de9c22c5bb8d907605eda5b792a14236dd6af3d
edi-sipka@gmail.com <edisipka@gmail.com>
2022-11-01 12:07:06 +0100

    Fix linters

--- person.rb
+++ person.rb
@@ -10,12 +10,12 @@ class Person
   end 
 
   def can_use_services?
-    is_age? || @parent_permission
+    of_age? || @parent_permission
   end
 
   private 
 
-  def is_age?
+  def of_age?
      @age >= 18
     end
 end
