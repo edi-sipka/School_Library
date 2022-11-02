722e7b29d976626b0f3b30d2f53c251befacea48
edi-sipka@gmail.com <edisipka@gmail.com>
2022-11-02 16:48:58 +0100

    Add classroom class

---
+++ classroom.rb
@@ -0,0 +1,13 @@
+class Classroom 
+  attr_accessor :label
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students.push(student) unless @students.include?(student)
+    student.classroom = self
+  end
+end
